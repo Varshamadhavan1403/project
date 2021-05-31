46bf1cb48f0ffa65e4457ba5f8400147b900f8b0
Varshamadhavan143 <varshamadhavan1622@gmail.com>
2021-05-31 18:32:39 +0530

    Add existing file

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import TaskList,TaskDetail,CreateTask
+from .views import TaskList,TaskDetail,CreateTask,UpdateTask,DeleteTask,CustomLoginView,RegisterPage
+from django.contrib.auth.views import LogoutView
 urlpatterns=[
+		path('login/',CustomLoginView.as_view(),name="login"),
+		path('logout/',LogoutView.as_view(next_page='login'),name="logout"),
+		path('register/',RegisterPage.as_view(),name="register"),
 		path('',TaskList.as_view(),name="tasks"),
 		path('task/<int:pk>/',TaskDetail.as_view(),name="task"),
 		path('create-task/',CreateTask.as_view(),name="create-task"),
+		path('update-task/<int:pk>/',UpdateTask.as_view(),name="update-task"),
+		path('delete-task/<int:pk>/',DeleteTask.as_view(),name="delete-task"),
 ]
